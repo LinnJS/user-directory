274c2a2b306353e45289dc0f14c4cf5e3dbdb414
JSLinn727 <justinslinn727@gmail.com>
2017-07-25 16:57:23 -0400

    Routes created, semi-functional, need to build out HTML and route to proper data.

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 const express = require('express')
 const app = express()
+const mustacheExpress = require('mustache-express')
+const data = require('./data')
+
+app.use(express.static('public'))
+
+app.engine('mustache', mustacheExpress())
+app.set('views', './templates')
+app.set('view engine', 'mustache')
+
+app.get("/", (req, res) => {
+  res.render("home", data)
+})
+
+app.get("/info.mustache:username")
+
+app.listen(3000, () => {
+  console.log('Listening to port 3000!')
+})
