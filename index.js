de22c3ba6e6d26b13d3050538af8af04d645e143
JSLinn727 <justinslinn727@gmail.com>
2017-07-28 09:36:49 -0400

    Set up user info page, next is to finish styling, and if else statments for empty data sets.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
-const app = express()
-const mustacheExpress = require('mustache-express')
 const data = require('./data')
+const mustacheExpress = require('mustache-express')
+const app = express()
 
 app.use(express.static('public'))
 app.engine('mustache', mustacheExpress())
